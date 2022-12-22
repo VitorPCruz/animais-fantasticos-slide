57aa106b2070d5ac1b13428415c3348478f7503e
Vitor Pereira <vitor-pcruz@hotmail.com>
2022-12-22 08:48:12 -0300

    custom controls estilizadas

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ import { Slide, SlideNav } from './slide.js';
 const SLIDE = new SlideNav('.slide', '.slide-wrapper');
 SLIDE.init();
 SLIDE.addArrow('.prev', '.next');
-SLIDE.addControl();
+SLIDE.addControl('.custom-controls');
